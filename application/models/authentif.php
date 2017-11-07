f9e1d509241be7b0527239df70e1db05e7a0f1ca
Spaituk <Spaituk_7@hotmail.com>
2017-11-07 16:49:31 +0100

    Test 4

--- application/models/authentif.php
+++ application/models/authentif.php
@@ -10,18 +10,18 @@ class Authentif extends CI_Model {
 
 	 /**
 	 * Teste si un quelconque visiteur est connecté
-	 * 
-	 * @return vrai ou faux 
+	 *
+	 * @return vrai ou faux
 	 */
 	public function estConnecte()
 	{
 	  return $this->session->userdata('idUser');
 	}
-	
+
 	/**
 	 * Enregistre dans une variable session les infos d'un visiteur
-	 * 
-	 * @param $id 
+	 *
+	 * @param $id
 	 * @param $nom
 	 * @param $prenom
 	 */
@@ -47,7 +47,7 @@ class Authentif extends CI_Model {
                    'nom' => '',
                    'prenom' => ''
 				);
-	
+
 		$this->session->unset_userdata($authUser);
 		$this->session->sess_destroy();
 
@@ -57,12 +57,12 @@ class Authentif extends CI_Model {
 
 	/**
 	 * Vérifie en base de données si les informations de connexions sont correctes
-	 * 
-	 * @return : renvoie l'id, le nom et le prenom de l'utilisateur dans un tableau s'il est reconnu, sinon un tableau vide.
+	 *
+	 * @return : renvoie l'id, le nom et le prenom de l'visiteur dans un tableau s'il est reconnu, sinon un tableau vide.
 	 */
-	public function authentifier ($login, $mdp) 
+	public function authentifier ($login, $mdp)
 	{	// TODO : s'assurer que les paramètres reçus sont cohérents avec ceux mémorisés en session
-	
+
 		$this->load->model('dataAccess');
 
 		$authUser = $this->dataAccess->getInfosVisiteur($login, $mdp);
