4a4032caf0150b6e03f7bac1450ac931c8cd5b7d
Spaituk <Spaituk_7@hotmail.com>
2017-11-21 13:53:11 +0100

    Correction de la vue connexion comptable pour que l'on puisse la différencier de l accueil connexion visiteur

--- application/views/v_coAccueil.php
+++ application/views/v_coAccueil.php
@@ -1,22 +1,23 @@
 <div id="contenu">
+	<h2>Vous etes connecté en tant que comptable</h2>
 	<h2>Gestion des frais des visiteurs</h2>
 	<p>Bienvenue dans votre application de gestion des frais de déplacements.</p>
 	<p>
 		Afin de rembource nos visiteur vous devez confirmer leurs fiche de frais.
 	</p>
 		<ul>
-			<li>Les fiches de frais sont redirig� automatiquement par l'application;</li>
+			<li>Les fiches de frais sont redirig� automatiquement par l'application;</li>
 			<li>Vous confirmere les fiches de frais a votre rythme;</li>
 		</ul>
-	
+
 
 	<p>
-		Au moyen du bandeau gauche, vous avez accès aux fonctionalités 
-		du profil comptable : 
+		Au moyen du bandeau gauche, vous avez accès aux fonctionalités
+		du profil comptable :
 	</p>
 		<ul>
 			<li>Gérer vos fiches de frais (modifier, signer, imprimer)</li>
 			<li>Se déconnecter</li>
 		</ul>
-	
+
 </div>
