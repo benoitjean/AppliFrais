4d1659c93fe77e0a8a945a4c981df600cffff912
Spaituk <Spaituk_7@hotmail.com>
2018-02-14 16:46:25 +0100

    Ajout dans la modification de ficher par un utilisateur de total/montant

--- application/views/v_visModListeFrais.php
+++ application/views/v_visModListeFrais.php
@@ -1,17 +1,22 @@
+
 <?php
 	$this->load->helper('url');
 ?>
 
 <div id="contenu">
 	<h2>Renseigner ma fiche de frais du mois <?php echo $numMois."-".$numAnnee; ?></h2>
-					
+
 	<?php if(!empty($notify)) echo '<p id="notify" >'.$notify.'</p>';?>
-	 
+
 	<form method="post"  action="<?php echo base_url("c_visiteur/majForfait");?>">
 		<div class="corpsForm">
-		  
+
 			<fieldset>
 				<legend>Eléments forfaitisés</legend>
+				<strong class="jvs">Quantité</strong>
+				<strong class="jvs">Montant</strong>
+				<strong class="jvs">Total</strong>
+
 				<?php
 					foreach ($lesFraisForfait as $unFrais)
 					{
@@ -19,7 +24,7 @@
 						$libelle = $unFrais['libelle'];
 						$quantite = $unFrais['quantite'];
 
-						echo 
+						echo
 						'<p>
 							<label for="'.$idFrais.'">'.$libelle.'</label>
 							<input type="text" id="'.$idFrais.'" name="lesFrais['.$idFrais.']" size="10" maxlength="5" value="'.$quantite.'" />
@@ -33,42 +38,42 @@
 			<p>
 				<input id="ok" type="submit" value="Enregistrer" size="20" />
 				<input id="annuler" type="reset" value="Effacer" size="20" />
-			</p> 
+			</p>
 		</div>
 	</form>
 
-	
+
 	<table class="listeLegere">
 		<caption>Descriptif des éléments hors forfait</caption>
 		<tr>
 			<th >Date</th>
-			<th >Libellé</th>  
-			<th >Montant</th>  
-			<th >&nbsp;</th>              
+			<th >Libellé</th>
+			<th >Montant</th>
+			<th >&nbsp;</th>
 		</tr>
-          
-		<?php    
-			foreach( $lesFraisHorsForfait as $unFraisHorsForfait) 
+
+		<?php
+			foreach( $lesFraisHorsForfait as $unFraisHorsForfait)
 			{
 				$libelle = $unFraisHorsForfait['libelle'];
 				$date = $unFraisHorsForfait['date'];
 				$montant=$unFraisHorsForfait['montant'];
 				$id = $unFraisHorsForfait['id'];
-				echo 
+				echo
 				'<tr>
 					<td class="date">'.$date.'</td>
 					<td class="libelle">'.$libelle.'</td>
 					<td class="montant">'.$montant.'</td>
 					<td class="action">'.
-					anchor(	"c_visiteur/supprFrais/$id", 
-							"Supprimer ce frais", 
+					anchor(	"c_visiteur/supprFrais/$id",
+							"Supprimer ce frais",
 							'title="Suppression d\'une ligne de frais" onclick="return confirm(\'Voulez-vous vraiment supprimer ce frais ?\');"'
 						).
 					'</td>
 				</tr>';
 			}
-		?>	  
-                                          
+		?>
+
     </table>
 
 	<form method="post" action="<?php echo base_url("c_visiteur/ajouteFrais");?>">
@@ -93,7 +98,7 @@
 			<p>
 				<input id="ajouter" type="submit" value="Ajouter" size="20" />
 				<input id="effacer" type="reset" value="Effacer" size="20" />
-			</p> 
+			</p>
 		</div>
 	</form>
 </div>
